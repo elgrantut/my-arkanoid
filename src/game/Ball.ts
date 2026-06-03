a2f204e3193685cb01d887bb9f37f448d934f394
Matias Sanchez <tute.sanchez@gmail.com>
2026-06-03 15:09:38 -0300

    lint and format config

---
+++ src/game/Ball.ts
@@ -0,0 +1,11 @@
+class Ball {
+  x = 400;
+  y = 300;
+
+  vx = 4;
+  vy = -4;
+
+  radius = 10;
+}
+
+export default Ball;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import './style.css'
+import "./style.css";
 
-const canvas = document.createElement('canvas')
+const canvas = document.createElement("canvas");
 
-canvas.width = 800
-canvas.height = 600
+canvas.width = 800;
+canvas.height = 600;
 
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext("2d");
 
-ctx?.fillRect(100, 100, 50, 50)
+ctx?.fillRect(100, 100, 50, 50);
 
-document.querySelector<HTMLDivElement>('#app')!.appendChild(canvas)
+document.querySelector<HTMLDivElement>("#app")!.appendChild(canvas);
