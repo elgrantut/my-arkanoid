73d8f762b59d7f97e69bb52c31089332ad45a7fc
Matias Sanchez <tute.sanchez@gmail.com>
2026-06-03 14:39:04 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+
+const canvas = document.createElement('canvas')
+
+canvas.width = 800
+canvas.height = 600
+
+const ctx = canvas.getContext('2d')
+
+ctx?.fillRect(100, 100, 50, 50)
+
+document.querySelector<HTMLDivElement>('#app')!.appendChild(canvas)
